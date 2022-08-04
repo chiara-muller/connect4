04aa5e394f7583cd783eab72d38dd5f8fbf96938
chiara-muller <chiaramuller@hotmail.be>
2022-08-04 13:41:41 +0200

    visual update

---
+++ js/p4.js
@@ -0,0 +1,115 @@
+let tableau = Array(6);
+
+for (let i = 0; i < 6; i ++) {
+    tableau[i] = Array(7);
+    for (let j = 0; j < 7; j ++) {
+        tableau[i][j] = ' ';
+    }
+}
+
+tableau[4][3] = 'x'
+tableau[5][3] = 'x'
+tableau[5][4] = '0'
+tableau[5][5] = '0'
+
+
+
+let game = [
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', 'x', 'x', ' ', ' ', ' '],
+  [' ', 'o', 'o', 'x', 'o', 'o', ' ']
+]
+
+function display(game) {
+    console.log("---------");
+
+    for ( let i in game) {
+        console.log("|" + game[i].join("") + "|")
+    }
+
+    console.log("---------")
+}
+
+display(game)
+
+// function insertCoin(game, column) {
+//     for (let i=5; i>=0; i--) {
+//         if (game[i][column] == " ") {
+//             game[i][column] = "x";
+//             break;
+//         } 
+//     }
+    
+//     return game
+// }
+
+function insertCoin(game, column) {
+    for (let i=5; i>=0; i--) {
+        if (game[i][column] == " ") {
+            game[i][column] = nextCoin(game);
+            break;
+        } 
+    }
+    
+    return game
+}
+
+//game = insertCoin(game, 3)
+//display(game)
+
+function nextCoin(game) {
+    const numberOfX = numberOfCoins(game, 'x')
+    const numberOfO = numberOfCoins(game, 'o')
+    
+    if (numberOfX < numberOfO) {
+        return 'x';
+    }
+    else {
+        return 'o';
+    }
+}
+
+// function numberOfCoins(game, coin) {
+//     let total = {}
+
+//     for (let row of game) {
+//         for (let letter of row) {
+//             if (total[letter]) {
+//                 total[letter] += 1;
+//             } else {
+//                 total[letter] = 1;
+//             }
+//         }
+//     }
+    
+//     return total[coin]
+// }
+
+function numberOfCoins(game, coin) {
+    let total = 0
+
+    for (let row of game) {
+        for (let letter of row) {
+            if (coin == letter) {
+              total += 1 
+            }
+        }
+    }
+
+    // for (let i = 0; i < 6; i ++) {
+    //     for (let j = 0; j < 7; j ++) {
+    //         if (game[i][j] == coin) {
+    //             total += 1
+    //         }
+    //     }
+    // }
+    
+    return total
+}
+
+numberOfCoins(game, 'o')
+
+
